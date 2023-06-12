2f51847c65da70df0d4e78275206626c0ebf2601
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-06-12 15:12:09 -0400

    added data to the seeds.sql for the database

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES
+('Engineering'),
+('Finance'),
+('Legal'),
+('Human Resources'),
+('Management'),
+('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Software Engineer', 120000, 1),
+('Account Manager', 100000, 2),
+('Lead Engineer', 90000, 3),
+('Project Manager', 95000, 4),
+('Accountant', 84000, 5),
+('Human Resources Manager' 70000, 6),
