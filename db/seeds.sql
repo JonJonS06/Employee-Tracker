e5713250cc36476034d00cff85c4f4ecdedafc5a
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-06-14 09:37:50 -0400

    created the connection file

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,4 +14,16 @@ VALUES
 ('Lead Engineer', 90000, 3),
 ('Project Manager', 95000, 4),
 ('Accountant', 84000, 5),
-('Human Resources Manager' 70000, 6),
+('Human Resources Manager', 70000, 6),
+('Receptionist', 55000, 7);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Marty', 'McFly', 2, 4),
+('Michael', 'Jordan', 5, NULL),
+('Jack', 'Sparrow', 3, 4),
+('Frodo', 'Baggins', 6, NULL),
+('Jill', 'Taylor', 7, 4),
+('Ronald', 'McDonald', 1, NULL),
+('Carlton', 'Whitfield', 4, NULL),
+('Tyler', 'Durden', 2, 4);
